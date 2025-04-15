3f52727fb19681e8496069fb937fed9727d5fbf0
Andy Lang <djdreviz@gmail.com>
2025-04-16 01:04:52 +0300

    15. Using variables - String

--- HelloWorld/HelloWorld/Program.cs
+++ HelloWorld/HelloWorld/Program.cs
@@ -1,2 +1,24 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, Andy!");
+Console.WriteLine("Hello, World!");
+
+// declare a variable
+string myFriendsName;
+
+// assign a value to the variable
+myFriendsName = "John";
+
+// declare and assign a variable in one line
+string mySistersName = "Mary";
+
+mySistersName = "Mary Jane";
+
+// print the value of the variable
+Console.WriteLine(myFriendsName);
+
+// overwrite the value of the variable
+myFriendsName = "Jane";
+
+// print the new value of the variable
+Console.WriteLine(myFriendsName);
+
+Console.ReadKey();
