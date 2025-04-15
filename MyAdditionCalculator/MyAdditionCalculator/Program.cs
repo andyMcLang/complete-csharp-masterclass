cf9625a669f65098bff257752059bb54e20a1c45
Andy Lang <djdreviz@gmail.com>
2025-04-16 02:17:02 +0300

    16. Understanding User Input and the Data Type String done!

---
+++ MyAdditionCalculator/MyAdditionCalculator/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Prints out whatever is in side of ()
+Console.Write("Kirjoita jottain: ");
+
+// takes the user input and stores it in a variable
+// Variable with the name "userInput" and the data type of string
+// strings are used to store text
+string userInput = Console.ReadLine();
+Console.WriteLine("Kirjoitit: " + userInput);
