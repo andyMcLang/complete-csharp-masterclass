61f824f1fc069bcccc72d8411a80235d89d8b177
Andy Lang <djdreviz@gmail.com>
2025-04-24 01:37:19 +0300

    36. Implicit Conversion done!

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,15 @@
+﻿// implicit conversion
+
+int myInt = 13;
+double myDouble = myInt;
+
+long myLong = 12;
+// not working:
+// myInt = myLong;
+
+float myFloat = 13.4f;
+myDouble = myFloat;
+
+Console.WriteLine("My Float:" + myFloat + " and My Double:" + myDouble);
+
+Console.ReadKey();
