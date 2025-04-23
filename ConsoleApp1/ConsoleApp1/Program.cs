fa260a58aee6f37fb19c2b1c5eae8693c98a3cbb
Andy Lang <djdreviz@gmail.com>
2025-04-24 01:49:01 +0300

    37. Explicit Conversion done!

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,15 +1,26 @@
 ﻿// implicit conversion
 
-int myInt = 13;
+int myInt = 1323450983;
 double myDouble = myInt;
 
-long myLong = 12;
-// not working:
-// myInt = myLong;
+long myLong = myInt;
+myLong = 132345098312389013;
 
-float myFloat = 13.4f;
-myDouble = myFloat;
+// explicit conversion
+int myInt2 = (int)myLong;
 
-Console.WriteLine("My Float:" + myFloat + " and My Double:" + myDouble);
+float myFloat = 123.144f;
+myDouble = 13.2123123123;
+
+// explicit conversion
+myFloat = (float)myDouble;
+
+int myInt3;
+double myDouble3 = 13.5;
+myInt3 = (int)myDouble3;
+
+
+Console.WriteLine("My Float: " + myFloat + " and My Double: " + myDouble);
+Console.WriteLine("My Int3: " + myInt3 + " and My Double3: " + myDouble3);
 
 Console.ReadKey();
