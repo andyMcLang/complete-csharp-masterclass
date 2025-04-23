124c0dd17b880f64dc3f8a0825df9eed28d464b1
Andy Lang <djdreviz@gmail.com>
2025-04-24 02:10:55 +0300

    39. Implicitly Typed Variables vs Explicitly Typed Variables done!

---
+++ DataTypes/DataTypes/Program.cs
@@ -0,0 +1,20 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// implicitely typed variable
+
+var myFavoriteGenre = "DramaComedy";
+
+var myFavoriteNumber = 7;
+
+// not working
+// myFavoriteGenre = 65;
+
+// Referency Type
+// explicitely typed variable
+string myName = "Denis";
+
+// defining or setting up a variable
+string petsName;
+
+// initialize variable
+petsName = "Daisy";
