babb6fb415a5a9fdc2cb6649f34ea062f9d97672
Andy Lang <djdreviz@gmail.com>
2025-04-25 01:55:44 +0300

    43. More String Manipulation with String Formatting done!

---
+++ MoreOperators/MoreOperators/Program.cs
@@ -0,0 +1,8 @@
+﻿
+int id = 157;
+double hinta = 19.95;
+string tuote = "Samsung-puhelimen kuoret";
+
+Console.WriteLine("id numero on {0} ja hinta on {1}. Tuote on sitten {2}", id, hinta, tuote);
+
+Console.ReadKey();
